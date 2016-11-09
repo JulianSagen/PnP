b8be08f222f1d7fa6cbb171fe330023c7d74cbc7
eirinm <Eirin@Eirins-MacBook-Pro.local>
2016-11-09 18:48:32 +0100

    Hamburger menyen.. men får ikke menyen for "smallscreen" til å dukke opp

---
+++ script.js
@@ -0,0 +1,19 @@
+(function() {
+
+  "use strict";
+
+  var toggles = document.querySelectorAll(".hamburger");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+
+})();
