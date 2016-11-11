d20b877eee1ca3d667b8d2ce6e8d464bbbf98e2f
eirinm <Eirin@vpn-158-36-145-194.vpn.hioa.no>
2016-11-11 12:22:19 +0100

    små endringer på menyen

--- script.js
+++ script.js
@@ -14,3 +14,52 @@
         this.style.borderBottom = (this.style.borderBottom == '3px solid transparent') ? '3px solid white' : '3px solid transparent';
     });
 
+
+
+
+    (function() {
+
+      "use strict";
+
+      var toggles = document.querySelectorAll(".c-hamburger");
+
+      for (var i = toggles.length - 1; i >= 0; i--) {
+        var toggle = toggles[i];
+        toggleHandler(toggle);
+      };
+
+
+
+      function toggleHandler(toggle) {
+        toggle.addEventListener( "click", function(e) {
+          e.preventDefault();
+          (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+        });
+      }
+
+
+    })();
+
+
+    /*Dropdown menu*/
+
+    	function myFunction() {
+    		document.getElementById("myDropdown").classList.toggle("show")
+    		;
+    	}
+
+    /*Close dropdown when clicking outside*/
+
+    	window.onclick = function(event) {
+      if (!event.target.matches("#hamburgerknapp")) {
+
+        var dropdowns = document.getElementsByClassName("smallscreen");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
