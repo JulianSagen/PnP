0529a04b76c79a1252d28c9916a6f0f06e69c9a9
teritry <teritry2@gmail.com>
2016-11-11 11:25:44 +0100

    Fixed menu bar javascript on click

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-<<<<<<< Updated upstream
     id1.addEventListener('click', function() {
         document.getElementById('id2').style.borderBottom = '3px solid transparent';
         document.getElementById('id3').style.borderBottom = '3px solid transparent';
@@ -15,5 +13,4 @@
         document.getElementById('id2').style.borderBottom = '3px solid transparent';
         this.style.borderBottom = (this.style.borderBottom == '3px solid transparent') ? '3px solid white' : '3px solid transparent';
     });
-=======
->>>>>>> Stashed changes
+
