429a4d83c95e1442f7e1ec33302a610b66ad62e7
teritry <teritry2@gmail.com>
2016-11-20 14:54:07 +0100

    R.I.P. Resize

--- script.js
+++
@@ -1,6 +0,0 @@
-function resizeText(multiplier) {
-    var element = document.body;
-    var currentSize = element.style.fontSize || 18;
-    element.style.fontSize = (parseFloat(currentSize) + (multiplier * 4)) + "px";
-    element.style.lineHeight = (parseFloat(currentSize) + (multiplier) * 4) + "px";
-}
